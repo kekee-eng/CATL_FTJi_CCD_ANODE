a9ad95d57b7dac43c713341d0e40238d8457f084
linfuci <fransis1535@qq.com>
2017-08-08 09:11:01 +0800

    v0.4

--- DetectCCD/EntryGrab.cs
+++ DetectCCD/EntryGrab.cs
@@ -3,8 +3,6 @@ using HalconDotNet;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static DetectCCD.DataGrab;
 
 namespace DetectCCD {
--- DetectCCD/UtilPerformance.cs
+++ DetectCCD/UtilPerformance.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace DetectCCD {
 
+    using System;
     using System.Diagnostics;
     using System.Management;
 
